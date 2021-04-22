9a22ff8a6f667b23080797536520fbf64f12fc07
rahulunair <rahulunair@gmail.com>
2021-04-22 16:54:42 -0700

    v9 pyt changes

--- dlrs/pytorch/scripts/torch_utils.sh
+++ dlrs/pytorch/scripts/torch_utils.sh
@@ -28,33 +28,33 @@ else
 	FBGEMM=1
 fi
 
-export GIT_BRANCH=v1.6.0 # Pytorch branch
-export GIT_TAG=v0.7.0    # Torchvision tag
+export TCH_TAG=v1.8.0-rc2 # Pytorch branch
+export VISION_TAG=master    # Torchvision tag
 export CFLAGS="-O3 -mfma -mtune=$TUNE -march=$ARCH"
 export CXXFLAGS="-O3 -mfma -mtune=$TUNE -march=$ARCH"
 export USE_FBGEMM=$FBGEMM
 export GCC_IGNORE_WERROR=1
 
-echo "torch version: $GIT_BRANCH"
-echo "torchvision version: $GIT_TAG"
+echo "torch version: $TCH_TAG"
+echo "torchvision version: $VISION_TAG"
 echo "building for: $ARCH"
 echo "tuning for: $TUNE"
 echo "fbgemm for int8: $FBGEMM"
 
-wget https://github.com/hongzhen1/pytorch/commit/3511d7f6bd2060e20cf77b770ae32ff538700f37.diff -O dataloader.diff
+#wget https://github.com/hongzhen1/pytorch/commit/3511d7f6bd2060e20cf77b770ae32ff538700f37.diff -O dataloader.diff
 echo "=================clone pytorch============================="
 mkdir /torch-wheels
 git clone https://github.com/pytorch/pytorch.git /buildir/pytorch \
-    && cd /buildir/pytorch && git checkout gh/xiaobingsuper/18/orig \
+    && cd /buildir/pytorch && git checkout ${TCH_TAG} \
     && python setup.py clean \
     && git submodule sync && git submodule update --init --recursive \
-    && git apply /buildir/dataloader.diff \
-    && cd third_party/ideep/ && git checkout master && git pull && git checkout pytorch_dnnl_dev && cd ../../ \
-    && git add third_party/ideep && git submodule sync &&  git submodule update --init --recursive \
-    && rm -rf caffe2/opt/onnxifi_op.* \
+    && git apply /buildir/dataloader.patch_v1.8.0-rc2 \
     && sed -i 's#^  ${CMAKE_CURRENT_SOURCE_DIR}/tensor_iterator_test.cpp##g' aten/src/ATen/test/CMakeLists.txt
+    #&& rm -rf caffe2/opt/onnxifi_op.* \
+#    && cd third_party/ideep/ && git checkout master && git pull && git checkout pytorch_dnnl_dev && cd ../../ \
+#    && git add third_party/ideep && git submodule sync &&  git submodule update --init --recursive \
 echo "==================done=========================================="
 
 echo "=================clone torchvison============================="
-git clone -b ${GIT_TAG} https://github.com/pytorch/vision.git /buildir/vision
+git clone -b ${VISION_TAG} https://github.com/pytorch/vision.git /buildir/vision
 echo "==================done=========================================="
