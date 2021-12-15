36f5f934f81b0b42715fbd67348d9dc836a10b73
Miguel Daniel Reyes <miguel.daniel.reyes.martinez@intel.com>
2021-12-15 16:28:57 -0600

    removing java based stacks eol (#108)

--- dsrs/spark/conf/spark/spark-env.sh
+++
@@ -1,3 +0,0 @@
-OPENBLAS_NUM_THREADS=1
-MKL_NUM_THREADS=1
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib64/:/usr/lib/native:/opt/intel/oneapi/mkl/latest/lib/intel64/:/opt/intel/oneapi/compiler/2021.2.0/linux/compiler/lib/intel64_lin/
--- dsrs/spark/scripts/spark-shell-mkl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# spark-shell with mkl enabled, to verify MKL is enabled:
-# On spark-shell:
-#                    import com.github.fommil.netlib.BLAS 
-#                    println(BLAS.getInstance().getClass().getName())
-MKL_WRAPPER="/opt/intel/oneapi/mkl/latest/wrapper/mkl_wrapper.jar"
-JAVA_OPTIONS="-Dcom.github.fommil.netlib.BLAS=com.intel.mkl.MKLBLAS -Dcom.github.fommil.netlib.LAPACK=com.intel.mkl.MKLLAPACK"
-
-spark-shell --conf "spark.executor.extraClassPath=$MKL_WRAPPER" --conf "spark.driver.extraClassPath=$MKL_WRAPPER" --conf "spark.executor.extraJavaOptions=$JAVA_OPTIONS" --conf "spark.driver.extraJavaOptions=$JAVA_OPTIONS"
